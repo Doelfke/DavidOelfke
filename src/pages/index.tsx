248637d17ac2d748b9db954862476951d8c38069
David Oelfke <doelfke@gmail.com>
2024-10-14 20:38:43 -0700

    Some copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,21 +6,44 @@ import styles from "./index.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.title}>
-      <TypeAnimation
-        preRenderFirstString
-        sequence={[
-          250,
-          "I'm David Oelfke and I'm",
-          250,
-          "I'm David Oelfke and I'm a web developer.",
-          250,
-          "I'm David Oelfke and I'm an engineering leader.",
-          250,
-          "I'm David Oelfke and I'm a software engineer.",
-        ]}
-        speed={50}
-      />
+    <div className={styles.page}>
+      <div className={styles.title}>
+        <TypeAnimation
+          preRenderFirstString
+          sequence={[
+            250,
+            "I'm David Oelfke and I'm",
+            250,
+            "I'm David Oelfke and I'm a web developer",
+            250,
+            "I'm David Oelfke and I'm an engineering leader",
+            250,
+            "I'm David Oelfke and I'm a software engineer",
+          ]}
+          speed={50}
+        />
+      </div>
+      <div className={styles.subTitle}>from San Diego, California.</div>
+
+      <div className={styles.tagLine}>
+        I've worked for 2 people startups to tech giants and everything in
+        between, specializing in all things web.
+      </div>
+
+      <div className={styles.techStack}>My favorite tech stack is:</div>
+      <ul className={styles.techStackList}>
+        <li>TypeScript</li>
+        <li>Node.js</li>
+        <li>React</li>
+        <li>NestJS</li>
+        <li>TypeORM</li>
+        <li>Postgres</li>
+        <li>Heroku</li>
+      </ul>
+
+      <div className={styles.seeMyWork}>
+        <a href="/resume">See my work</a>
+      </div>
     </div>
   );
 }
