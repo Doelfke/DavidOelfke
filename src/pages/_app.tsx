7972095cbeeb670fe7be1163a32bc1204813b96f
David Oelfke <doelfke@gmail.com>
2024-10-22 13:12:53 -0700

    Page loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import RootLayout from "../components/layout";
+import { PagesProgressBar } from "next-nprogress-bar";
 import "dotenv/config";
 
 import "./globals.css";
@@ -17,7 +18,18 @@ type AppPropsWithLayout = AppProps & {
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout =
-    Component.getLayout || ((page) => <RootLayout>{page}</RootLayout>);
+    Component.getLayout ||
+    ((page) => (
+      <RootLayout>
+        {page}
+        <PagesProgressBar
+          height="2px"
+          color="#9333ea"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+      </RootLayout>
+    ));
 
   return getLayout(<Component {...pageProps} />);
 }
