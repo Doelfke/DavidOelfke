fd7fe673274811054eb64253b4ed4b91d50225da
David Oelfke <doelfke@gmail.com>
2024-10-23 10:29:55 -0700

    better sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,71 @@
+import BlogPost from "@/types/blogPost";
+import { blogUtils } from "@/utils/blogUtils";
+import * as contentful from "contentful";
+import type { IncomingMessage, ServerResponse } from "http";
+
+interface BlogPosts {
+  total: number;
+  limit: number;
+  skip: number;
+
+  items: BlogPost[];
+}
+
+function generateSiteMap(urls: string[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       <url>
+         <loc>https://davidoelfke.com</loc>
+       </url>
+       <url>
+         <loc>https://davidoelfke.com/about-me</loc>
+       </url>
+       <url>
+         <loc>https://davidoelfke.com/blog</loc>
+       </url>
+       <url>
+         <loc>https://davidoelfke.com/my-work</loc>
+       </url>
+       ${urls
+         .map((url) => {
+           return `
+         <url>
+             <loc>${`${url}`}</loc>
+         </url>
+       `;
+         })
+         .join("")}
+     </urlset>
+   `;
+}
+function SiteMap() {}
+
+export async function getServerSideProps(r: {
+  res: ServerResponse<IncomingMessage>;
+}) {
+  const client = contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE as string,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+  });
+
+  const result = (await client.getEntries()) as unknown as BlogPosts;
+
+  const pages = result.items.map((post) => {
+    return (
+      "https://davidoelfke.dev/" +
+      blogUtils.generateUrl(post.sys.id, post.fields.title)
+    );
+  });
+
+  const sitemap = generateSiteMap(pages);
+
+  r.res.setHeader("Content-Type", "text/xml");
+  r.res.write(sitemap);
+  r.res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
