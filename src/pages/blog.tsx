398195ea9fb70af15c70b6040a78ab91916809fd
David Oelfke <doelfke@gmail.com>
2024-10-24 08:18:08 -0700

    caching part 1

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,6 +5,7 @@ import styles from "./blog.module.scss";
 import { dateUtils } from "@/utils/dateUtils";
 import BlogPost from "@/types/blogPost";
 import { blogUtils } from "@/utils/blogUtils";
+import { Redis } from "@upstash/redis";
 
 interface BlogPosts {
   total: number;
@@ -24,7 +25,15 @@ export const getServerSideProps = async () => {
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
   });
 
+  const redis = Redis.fromEnv();
+  const cachedPosts = await redis.get<BlogPosts>("posts");
+
+  if (cachedPosts) {
+    return { props: { posts: cachedPosts } };
+  }
+
   const posts = (await client.getEntries()) as unknown as BlogPosts;
+  await redis.set("posts", posts);
 
   return { props: { posts } };
 };
