972652ac5ebf0be0a3da8bc64c83bfd23f90203c
David Oelfke <doelfke@gmail.com>
2025-11-09 17:52:44 -0800

    more data for AI

--- api/main.py
+++ api/main.py
@@ -35,7 +35,23 @@ def get_david_oelfke_info() -> dict:
         "name": "David Oelfke",
         "city": "San Diego",
         "programming languages and technologies": TECHNOLOGIES,
-        "work experience": WORK_HISTORY
+        "work experience": WORK_HISTORY,
+        "contact": {
+            "LinkedIn": "https//www.linkedin.com/in/davidoelfke",      
+        },
+        "education": [
+            {"school" :"Massachusetts Institute of Technology", "program": "AI 101"},
+            {"school" :"Harvard Business School", "program": "Resilient Leadership"},
+            {"school" :"Harvard Business School", "program": "Exercising Leadership: Foundational Principles"},
+            {"school" :"Fitchburg State University", "program": "CSC 1000 - Intro to Programming"},
+            {"school" :"Fitchburg State University", "program": "CSC 1400 - Computer Info Systems"},
+            {"school" :"Fitchburg State University", "program": "CSC 1500 - Computer Science I"},
+            {"school" :"Leominster High School", "program": "C++"},
+            {"school" :"Leominster High School", "program": "Visual Basic"},
+            {"school" :"Leominster High School", "program": "A+ Certification"},
+            {"school" :"Leominster High School", "program": "CCNA Certification"},
+
+        ]
     }
 
 @app.get("/")
